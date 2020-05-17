dc7a12a8597826a38626e596d1d798b08ecf49b3
mamasagung <65334552+mamasagung@users.noreply.github.com>
2020-05-17 18:10:01 +0700

    Add files via upload

---
+++ HACKER11.py
@@ -0,0 +1,96 @@
+#!/system/xbin/bash
+#MauNgapainLolz?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthotGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "T00Ls" | lolcat
+toilet -f big -F gay "HACKER11" 
+echo ""      
+echo "\033[32;1mAuthor : Mr.HACKER11 Feat Mr.IM7V" 
+echo "\033[35;1mTeam : HACKER INDONESIA"
+echo "\033[33;1mKontak Me :  087835334733"
+echo "\033[31;1mFOLLOW ME IG =MAMAS_AGUNG_2019:"
+echo "\033[33;1mHACKER INDONESIA" "\033[32;1m&" "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[============================[>".
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Spam IndiHome"
+echo "[===============================================]>"
+echo $purple "2.> Spam PHD"
+echo "[===============================================]>"
+echo "\033[32;1m"
+read -p "[root@T00Ls-HACKER11:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.HACKER11" | lolcat
+cd module
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.HACKER11"
+cd module
+php t.php
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.HACKER11" 
+apt update && upgrade
+apt install python2
+apt install git
+git clone https://github.com/mamasagung/hacker11
+cd perusak.py
+python2 perusak.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f standard -F gay "Mr.HACKER11" 
+apt update && upgrade
+apt install python2
+apt install git
+git clone https://github.com/mamasagung/hacker11
+cd ripper.py
+python2 ripper.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mDARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
